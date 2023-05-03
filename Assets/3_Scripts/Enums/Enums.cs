713e635fba9729475b213b9f3d15ea04aac6074c
Oguzhan Delibas <oguzhan.delibass@gmail.com>
2023-05-03 15:06:57 +0300

    +add: tmpro, uimanager, uipaneldata

---
+++ Assets/3_Scripts/Enums/Enums.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Zenith.Enums
+{
+    public enum MainPanels
+    {
+        WELCOME,
+        WELCOME_LOADING,
+        LOBBY
+    }
+
+    public enum PopUpPanels
+    {
+        LOGIN,
+        SIGNUP,
+        ERROR_NETWORK
+    }
+}
---
+++ Assets/3_Scripts/UI_Manager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenith.Enums;
+
+namespace Zenith.Managers
+{
+    public class UIManager : Singleton<UIManager>
+    {
+        
+
+        public void PanelActiveness()
+        {
+            
+        }
+
+    }
+}
+
